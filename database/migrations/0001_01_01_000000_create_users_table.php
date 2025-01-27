56ee3554604c392404a4707e89dddb0c5b963691
SohanKafle <soka_bca2077@lict.edu.np>
2025-01-27 14:31:59 +0545

    admin seeder and landing registration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('companyname')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('status')->default('new');
+            $table->string('role')->default('user');
+            $table->enum('type',['it','sales','bank'])->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
