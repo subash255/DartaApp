bb4c06921b125f58a3eaa31977bd14b781aa3c28
subash255 <suad_bca2077@lict.edu.np>
2025-02-07 15:47:53 +0545

    map implementation

--- resources/views/user/company/step1.blade.php
+++ resources/views/user/company/step1.blade.php
@@ -59,7 +59,7 @@
 
                     
                 </div>
-                <a href="{{ route('user.company.step2') }}"
+                <a href="{{ route('user.company.step2',$user->id) }}"
                     class="px-4 py-2 bg-orange-500 text-white rounded-lg hover:bg-orange-600 focus:outline-none focus:shadow-outline">Next</a>
             </form>
         </div>
